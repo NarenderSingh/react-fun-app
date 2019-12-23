b79331370063a72d68e720c250fb1d609336b14f
NarenderSingh <naren.delhi13@gmail.com>
2019-12-23 22:53:43 +0530

    Functional Component in React

--- src/index.js
+++ src/index.js
@@ -8,35 +8,32 @@ let skiData = {
   goal: 100
 };
 
-class SkiDayCounter extends Component {
-  getPercent = decimal => {
-    return decimal * 100 + "%";
-  };
+const getPercent = decimal => {
+  return decimal * 100 + "%";
+};
 
-  calcGoalProgress = (total, goal) => {
-    return this.getPercent(total / goal);
-  };
+const calcGoalProgress = (total, goal) => {
+  return getPercent(total / goal);
+};
 
-  render() {
-    const { total, powder, backcountry, goal } = this.props;
-    return (
-      <section>
-        <div>
-          <p>Total Days : {total}</p>
-        </div>
-        <div>
-          <p>Powder Days : {powder}</p>
-        </div>
-        <div>
-          <p>Backcountry Days : {backcountry}</p>
-        </div>
-        <div>
-          <p>Goal Progress : {this.calcGoalProgress(total, goal)}</p>
-        </div>
-      </section>
-    );
-  }
-}
+const SkiDayCounter = ({ total, powder, backcountry, goal }) => {
+  return (
+    <section>
+      <div>
+        <p>Total Days : {total}</p>
+      </div>
+      <div>
+        <p>Powder Days : {powder}</p>
+      </div>
+      <div>
+        <p>Backcountry Days : {backcountry}</p>
+      </div>
+      <div>
+        <p>Goal Progress : {calcGoalProgress(total, goal)}</p>
+      </div>
+    </section>
+  );
+};
 
 render(
   <SkiDayCounter
